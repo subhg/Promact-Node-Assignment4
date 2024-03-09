7ea4fa34168922df0bd2360b03bf78e1ae244d9b
subhg <113555022+subhg@users.noreply.github.com>
2024-03-09 21:56:37 +0530

    added comment

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
+// Import the Express framework
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set the view engine to EJS
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
 
+// Array to store user data
 const users = [];
+
+// Route for the home page
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+// Route for handling user creation via POST request
 app.post("/user", (req, res) => {
   const username = req.body.username;
   users.push(username);
   res.redirect("/users");
 });
 
+// Route for displaying the list of users
 app.get("/users", (req, res) => {
   res.render("users", { users });
 });
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
